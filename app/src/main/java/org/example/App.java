f54ac8d75da3e8b4a40f38827ca0b45b665679b4
Nick Spizzirri <Nick.spizzirri91993@gmail.com>
2025-01-26 21:34:56 -0600

    driver + methods
    learned about scanners

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,18 @@
 package org.example;
-
+import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+    System.out.println("Welcome! It's time to convert some ounces.");
+    Scanner myFirstScanner = new Scanner(System.in);
+    System.out.print("How many ounces are we talking here? ");
+    int oz = myFirstScanner.nextInt();
+    System.out.println("Alright.. Got it.. Let's do this.");
+    Converter converter = new Converter();
+    String intAndRemain = converter.toPoundsAndOunces(oz);
+    String doublelbs = converter.toPounds(oz);
+    System.out.println(oz + " oz equals " + intAndRemain +".");
+    System.out.println(oz + " oz equals " + doublelbs +".");
+    System.out.println("Thanks for checking out my program. Have a great day.");
+    myFirstScanner.close();
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -2,4 +2,14 @@ package org.example;
 
 public class Converter {
   // Your code goes here
+  public String toPounds(int oz){
+    double lbs = oz / 16.0;
+    return String.format("%.4f lbs", lbs);
+  }
+
+  public String toPoundsAndOunces(int oz){
+    int lbs = oz / 16;
+    int remainder = oz % 16;
+    return String.format("%d lbs %d oz", lbs, remainder);
+  }
 }
